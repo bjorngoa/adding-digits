0d84f28f81432f68f95a2abfc20b545a87e31b05
bjornole-kodehode <93578742+bjornole-kodehode@users.noreply.github.com>
2022-06-23 12:44:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [input, setInput] = useState({
+    number: ""
+  });
+  const [result, setResult] = useState("");
+  const handleInput = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const add = () => {
+    const { number } = input;
+    setResult(
+      String(number)
+        .split("")
+        .reduce((acc, curr) => {
+          return acc + Number(curr);
+        }, 0)
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1>Adding digits!</h1>
+      <span style={{ border: "1px red solid", padding: "5px" }}>
+        ex. 123 => 1+2+3 = 6
+      </span>
+      <br />
+      <br />
+      <input
+        onChange={handleInput}
+        name="number"
+        type="number"
+        style={{
+          marginRight: "10px",
+          padding: "10px",
+          border: "none",
+          borderRadius: "10px"
+        }}
+        placeholder="Enter digits here!"
+      />
+      <button
+        onClick={add}
+        style={{
+          backgroundColor: "blue",
+          border: "none",
+          borderRadius: "8px",
+          padding: "10px",
+          color: "#fff",
+          fontWeight: "bold"
+        }}
+      >
+        SUBMIT
+      </button>
+      <br />
+      <span>Result: {result}</span>
+    </div>
+  );
+}
