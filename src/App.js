111b9e6f94105f481f56a36d0c98154a134246d1
bjornole-kodehode <93578742+bjornole-kodehode@users.noreply.github.com>
2022-06-23 13:28:53 +0200

    styling added

--- src/App.js
+++ src/App.js
@@ -32,33 +32,58 @@ export default function App() {
       </span>
       <br />
       <br />
-      <input
-        onChange={handleInput}
-        name="number"
-        type="number"
+
+      <div
         style={{
-          marginRight: "10px",
-          padding: "10px",
-          border: "none",
-          borderRadius: "10px"
-        }}
-        placeholder="Enter digits here!"
-      />
-      <button
-        onClick={add}
-        style={{
-          backgroundColor: "blue",
-          border: "none",
-          borderRadius: "8px",
-          padding: "10px",
-          color: "#fff",
-          fontWeight: "bold"
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          width: "400px"
         }}
       >
-        SUBMIT
-      </button>
-      <br />
-      <span>Result: {result}</span>
+        <div>
+          <input
+            onChange={handleInput}
+            name="number"
+            type="number"
+            style={{
+              marginRight: "10px",
+              padding: "10px",
+              border: "none",
+              borderRadius: "10px"
+            }}
+            placeholder="Enter digits here!"
+          />
+          <button
+            onClick={add}
+            style={{
+              backgroundColor: "blue",
+              border: "none",
+              borderRadius: "8px",
+              padding: "10px",
+              color: "#fff",
+              fontWeight: "bold"
+            }}
+          >
+            SUBMIT
+          </button>
+        </div>
+        <div
+          style={{
+            border: "2px solid blue",
+            padding: "10px 10px 2px 2px",
+            backgroundColor: "#fff",
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100px",
+            marginTop: "10px",
+            marginLeft: "100px"
+          }}
+        >
+          <span>Result: </span>
+          <span>{result}</span>
+        </div>
+      </div>
     </div>
   );
 }
